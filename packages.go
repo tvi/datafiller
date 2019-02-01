278e1185302deb3078c65d373168cda35e39695e
fwieffering <frederick.wieffering@target.com>
2019-02-01 10:42:41 -0600

    Add null packages

--- packages.go
+++ packages.go
@@ -4,10 +4,25 @@ import (
 	"math/rand"
 	"reflect"
 	"time"
+
+	null "gopkg.in/guregu/null.v3"
 )
 
 var packages = make(map[string]reflect.Value)
 
 func packagesInit() {
+	randSeed := rand.New(rand.NewSource(-1))
 	packages["time.Time"] = reflect.ValueOf(time.Unix(rand.Int63n(2000000000), 0))
+	// guregu/null structs have Valid field that is a bool.
+	// this field was occasionally randomly set to false, meaning whatever value was passsed
+	// would be interpreted as null. Always want some value, so always set valid = true for null packages
+	packages["null.String"] = reflect.ValueOf(null.NewString("test", true))
+	packages["null.Int"] = reflect.ValueOf(null.NewInt(rand.Int63n(100), true))
+	if randSeed.Int63n(2) == 0 {
+		packages["null.Bool"] = reflect.ValueOf(null.NewBool(false, true))
+	} else {
+		packages["null.Bool"] = reflect.ValueOf(null.NewBool(true, true))
+	}
+	packages["null.Float"] = reflect.ValueOf(null.NewFloat(float64(randSeed.Float32()), true))
+	packages["null.Time"] = reflect.ValueOf(null.NewTime(time.Unix(rand.Int63n(2000000000), 0), true))
 }
